01f02cc104e355652d88415418493e363c68b0d3
mohamedeliwa <mohamed.av11@gmail.com>
2022-12-10 04:33:20 +0200

    converting the input into number

--- src/main.rs
+++ src/main.rs
@@ -18,5 +18,7 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
 }
