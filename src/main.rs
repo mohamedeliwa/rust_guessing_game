73d820596eb78e707f34f07df953036de65be4f9
mohamedeliwa <mohamed.av11@gmail.com>
2022-12-10 04:41:52 +0200

    create more explanatory message

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     println!("The secret number is: {secret_number}");
 
     loop {
-        println!("Please input your guess.");
+        println!("Please input your guess. from 1 to 100");
 
         // defining a mutable empty string
         let mut guess = String::new();
@@ -21,7 +21,10 @@ fn main() {
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Please input a number from 1 to 100!");
+                continue;
+            }
         };
 
         println!("You guessed: {guess}");
